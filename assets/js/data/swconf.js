662a0ff883a6e21a4fbc8af2111ec40b4e58f5e7
solutions418 <solutions418@gmail.com>
2025-02-16 22:02:03 -0500

    update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739662648',resources: [
+    cacheName: 'chirpy-1739761244',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
